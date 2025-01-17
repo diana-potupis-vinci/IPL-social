07e41edc4669220a6d7ea6d26fe11b652e9d4350
diana.potupis <diana.potupis@student.vinci.be>
2025-01-17 17:09:25 +0100

    refactor: passwordChecker to use regex for special character validation and simplify logic; update tests accordingly

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,19 +1,11 @@
 function passwordChecker(password) {
 
-  const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"];
+  const specialCharacters = /[!@#$%^&*]/;
   const hasLetter = /[a-zA-Z]/.test(password);
+  
+  const hasSpecialCharacter = specialCharacters.test(password);
 
-  if (!hasLetter) {
-    return false;
-  }
-
-  for (let i = 0; i < specialCharacters.length; i++) {
-    if (password.includes(specialCharacters[i]) && password.length >= 8) {
-      return true;
-    }
-  }
-
-  return false;
+  return hasLetter && hasSpecialCharacter && password.length >= 8;
 }
 
 module.exports = passwordChecker;

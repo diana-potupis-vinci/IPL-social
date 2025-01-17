265be5646aeb51e109c84f3cc75ce2db45ac0583
diana.potupis <diana.potupis@student.vinci.be>
2025-01-17 17:14:38 +0100

    refactor: streamline special character validation in passwordChecker function

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,13 +1,10 @@
 function passwordChecker(password) {
 
-  const specialCharacters = /[!@#$%^&*]/;
-
   const hasLetter = /[a-zA-Z]/.test(password);
 
-  const hasSpecialCharacter = specialCharacters.test(password);
+  const hasSpecialCharacter = /[!@#$%^&*]/.test(password);
 
   const hasIPL = password.toLowerCase().includes('ipl');
-
 
   return hasLetter && hasSpecialCharacter && password.length >= 8 && !hasIPL;
 }

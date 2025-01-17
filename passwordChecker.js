cf9373b35aeaeb1dca1cc6b3d91d235ed3964a56
diana.potupis <diana.potupis@student.vinci.be>
2025-01-17 16:56:29 +0100

    Enhance passwordChecker to require at least one special character and update tests

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,6 +1,14 @@
 function passwordChecker(password) {
-    return password.length >= 8;
+
+  const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"];
+
+  for (let i = 0; i < specialCharacters.length; i++) {
+    if (password.includes(specialCharacters[i]) && password.length >= 8) {
+      return true;
+    }
+  }
+
+  return false;
 }
-  
-  module.exports = passwordChecker;
-  
+
+module.exports = passwordChecker;
